91c56ee71a20aa48a21c91d62bd0cf61b0c2745a
Pedromallcher <pedromallcher@gmail.com>
2024-10-14 15:16:27 -0300

    feat(pasta home)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {HomeDesktop} from './screens/desktop/appDesktop'
+import {HomeDesktop} from './screens/desktop/home/appDesktop'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(

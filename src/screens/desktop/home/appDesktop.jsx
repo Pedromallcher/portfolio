975dca91fcf9539603b273a6afa1e1af08443741
Pedromallcher <pedromallcher@gmail.com>
2024-10-14 15:36:15 -0300

    feat(...)

--- src/screens/desktop/home/appDesktop.jsx
+++ src/screens/desktop/home/appDesktop.jsx
@@ -6,22 +6,22 @@ export const HomeDesktop = () => {
         <div className="content-home-desktop">
             
         <div className='box-principal'>
-            <div className='box-perfil'></div>
+            <div className='box-perfil'><h3>Bloco1</h3></div>
             <div className='box-content-4'>
                 <div className='bloco1'>
                     <div className='bloco1-1'>
-                        <div className='bloco1-2'>bloco1</div>
-                        <div className='bloco1-3'>bloco1</div>
+                        <div className='bloco1-2'>bloco2</div>
+                        <div className='bloco1-3'>bloco3</div>
                     </div>
                     <div className='bloco2-1'>
-                        <div className='bloco2-2'>bloco1</div>
-                        <div className='bloco2-3'>bloco1</div>
+                        <div className='bloco2-2'>bloco4</div>
+                        <div className='bloco2-3'>bloco5</div>
 
                        
                     </div>
 
                     <div className='bloco3-1'>
-                        <div className='bloco3-2'>bloco1</div>
+                        <div className='bloco3-2'>bloco6</div>
                        
                     </div>
 
@@ -31,8 +31,8 @@ export const HomeDesktop = () => {
                 </div>
 
                 <div className='bloco4-1'>
-                    <div className='bloco4-2'><h1>ola</h1></div>
-                    <div className='bloco4-3'><h1>ola</h1></div>
+                    <div className='bloco4-2'><h1>bloco7</h1></div>
+                    <div className='bloco4-3'><h1>bloco8</h1></div>
 
                 </div>
                 

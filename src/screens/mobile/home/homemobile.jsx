52d1de6e659728fa5ff396fe11297029ca42aca8
Pedromallcher <pedromallcher@gmail.com>
2024-10-25 18:48:29 -0300

    feat(box-perfil-mobile)

--- src/screens/mobile/home/homemobile.jsx
+++ src/screens/mobile/home/homemobile.jsx
@@ -1,12 +1,26 @@
 import '../globalmobile.css'
+import './homestyle.css'
 import { NavbarMobile } from '../../../components/mobile/navbar/navbar'
+
+//imports
+import perfilhomemobile from '../../../assets/svg-removebg-preview.png'
 
 export const HomeMobile = () => {
     return (
         <div className="content-mobile">
             <NavbarMobile/>
             <div className='homemobile-content'>
-                <h3>Ola</h3>
+               
+                <div className='box-perfil-mobile'>
+                    <div className='img-perfil-mobile'>
+                        <img src={perfilhomemobile} alt="" />
+                        <h3>@pedromallcher</h3>
+
+                    </div>
+                    
+
+
+                </div>
             </div>
 
         </div>

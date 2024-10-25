f7807abe0484c4e6a89782ef563148e757e0c027
Pedromallcher <pedromallcher@gmail.com>
2024-10-24 21:47:37 -0300

    feat(..)

--- src/routes/routesmobile.jsx
+++ src/routes/routesmobile.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes , Route } from "react-router-dom";
 //import components
-import { HomeMobile } from "../screens/mobile/homeMobile";
+import { HomeMobile } from "../screens/mobile/homemobile.jsx";
 
 
 

73ceb5ecdf90e996a2537e37759563e73c3fd138
Pedromallcher <pedromallcher@gmail.com>
2024-10-28 11:27:20 -0300

    feat(...)

--- src/screens/mobile/projectmobile/projectmobile.jsx
+++ src/screens/mobile/projectmobile/projectmobile.jsx
@@ -1,11 +1,70 @@
 import '../globalmobile.css'
+import '../home/homestyle.css'
+import './projectstyle.css'
 import { NavbarMobile } from '../../../components/mobile/navbar/navbar'
-
+import { FaExternalLinkAlt } from "react-icons/fa";
 export const ProjectMobile = () => {
     return (
 
-        <div className="">
+        <div className="content-mobile">
             <NavbarMobile/>
+
+        <div className='projectmobile-content'>
+        <div className='title-project'>
+                <h1>Projetos</h1>
+            </div>
+
+
+            <div className='box-projects-mobile'>
+                <div className='b1-projects'>
+
+                    <div className='img-b'></div>
+                    <div className='legend-projects'>
+                        <p>React</p>
+                        <p>Css</p>
+                        <p>Js</p>
+
+                    </div>
+
+                    <div className='title-project-mobile'>
+                        <h3>Project</h3>
+                        <FaExternalLinkAlt className='icon-title-project'/>
+                    </div>
+                </div>
+
+                <div className='b2-projects'>
+                <div className='img-b'></div>
+                <div className='legend-projects'>
+                        <p>React</p>
+                        <p>Css</p>
+                        <p>Js</p>
+
+                    </div>
+                <div className='title-project-mobile'>
+                        <h3>Project</h3>
+                        <FaExternalLinkAlt className='icon-title-project'/>
+                    </div>
+                </div>
+
+                <div className='b3-projects'>
+                <div className='img-b'></div>
+                <div className='legend-projects'>
+                        <p>React</p>
+                        <p>Css</p>
+                        <p>Js</p>
+
+                    </div>
+                <div className='title-project-mobile'>
+                        <h3>Project</h3>
+                        <FaExternalLinkAlt className='icon-title-project'/>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+          
         </div>
 
     )

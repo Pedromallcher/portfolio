a5347b9678217daa6ef58d972536db359217c9d5
Pedromallcher <pedromallcher@gmail.com>
2024-11-04 21:03:05 -0300

    feat(...)

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,36 @@ function App() {
   const [isMobileDevice, setIsMobileDevice] = useState(false);
 
   useEffect(() => {
-    // Função para verificar o tamanho da tela e definir o estado
+    // Função para verificar o tamanho da tela e a orientação para definir o estado
     const handleResize = () => {
-      setIsMobileDevice(window.innerWidth <= 768); // Definindo 768px como o limite para mobile
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      // Verifica se está entre 481px e 767px e em modo retrato (altura maior que largura)
+      const isMobile = width >= 481 && width <= 767 && height > width;
+      setIsMobileDevice(isMobile);
     };
 
-    // Chama a função ao carregar a página
+    // Chama a função ao carregar a página para definir o estado corretamente
     handleResize();
 
-    // Adiciona o listener de redimensionamento para ajustar se a tela mudar de tamanho
+    // Adiciona os listeners para resize e orientação
     window.addEventListener('resize', handleResize);
+    window.addEventListener('orientationchange', handleResize);
 
-    // Remove o listener quando o componente desmonta
+    // Remove os listeners quando o componente desmonta
     return () => {
       window.removeEventListener('resize', handleResize);
+      window.removeEventListener('orientationchange', handleResize);
     };
   }, []);
 
   return (
     <>
-    
       {isMobileDevice ? (
-        <RenderMobile /> // Renderiza para mobile
+        <RenderMobile /> // Renderiza para dispositivos móveis extra pequenos
       ) : (
-        <RenderDesktop /> // Renderiza para desktop
+        <RenderDesktop /> // Renderiza para desktop ou outros tamanhos
       )}
     </>
   );
